300bdfa95d2a11dacdfb8147967fa7bc610311f5
ShonhTan <thanh-son-vincent.pham@hetic.net>
2018-02-15 19:10:21 +0100

    correction style action

--- actions/action_update.php
+++ actions/action_update.php
@@ -3,6 +3,11 @@
 require_once "../init_db.php";
 // selectionne ine ligne de la table pour en modifier le nom le genre et l'année
 
+//Vérifie que les champs ne sont pas vides et renvoie vers la page admin si c'est le cas
+if (!isset($_POST['name']) || !isset($_POST['genre']) || !isset($_POST['year']) || !isset($_POST['episode']) || !isset($_POST['synopsis']) || $_POST['name']==="" || $_POST['genre']==="" || $_POST['year']==="" || $_POST['episode']==="" || $_POST['synopsis']==="") {
+    header('Location: ../admin.php?error_input');
+    exit();
+}
 
 $sql = "UPDATE
       `anime`
